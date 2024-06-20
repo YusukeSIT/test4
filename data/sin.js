d7fd4d1318dd7edda182fe02b3a02f5b37a08650
YusukeSIT <151612445+YusukeSIT@users.noreply.github.com>
2024-06-21 08:07:36 +0900

    Add files via upload

--- data/sin.js
+++ data/sin.js
@@ -11,7 +11,7 @@ function hanabi(x, y, z)
 
     // 爆発する座標
     let bomPosX = x;
-    let bomPosY = y+20;
+    let bomPosY = y+30;
     let bomPosZ = z;
 
 
@@ -21,7 +21,7 @@ function hanabi(x, y, z)
     {
         hinoPos[i] = new Array(3);
         hinoPos[i][0] = x;
-        hinoPos[i][1] = bomPosY - 20;
+        hinoPos[i][1] = bomPosY - 10;
         hinoPos[i][2] = z;
 
         hinoVec[i] = new Array(3);
@@ -117,5 +117,5 @@ function hanabi(x, y, z)
                 
             }
             upMove();
-        })
+        }, 10)
 }
